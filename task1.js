9d57ed2843a4a9c1726a670cd9455594d853c49c
NadyaGlushchenko <Nadyaglushchenko@ukr.net>
2020-07-27 23:11:30 +0300

    выполняет дз модуля 1

---
+++ task1.js
@@ -0,0 +1,9 @@
+const name = "«Генератор защитного поля»";
+let price = 1000;
+
+let firstMessage = `Выбран ${name} цена за штуку ${price} кредитов`;
+console.log(firstMessage);
+
+price = 2000;
+let secondMessage = `Выбран ${name} цена за штуку ${price} кредитов`;
+console.log(secondMessage);
